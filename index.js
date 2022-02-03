e27bae603462977c98a40fc63e44a64f5db40050
Prerit Yadav <preritcs2019@gmail.com>
2022-02-03 17:19:33 +0530

    added cards grid with responsiveness

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const getOffset = () => {
     const topDist = window.scrollY;
     let res = 100;
-    res = Math.max(90,res-(topDist/50));
+    res = Math.max(90, res - (topDist / 50));
     return res;
 }
 
 const getOpacity = () => {
     const topDist = window.scrollY;
     let op = 0;
-    op = Math.min(0.5,topDist/300);
+    op = Math.min(0.5, topDist / 300);
     return op;
 }
 
@@ -18,7 +18,7 @@ window.onload = () => {
     nav.style.width = wid + "%";
     nav.style.backgroundImage = 'linear-gradient(90deg,rgba(255, 238, 151,' + op + '),rgba(251, 228, 255,' + op + '))';
     nav.style.backgroundImage = '-webkit-gradient(linear,left top,right top,from(rgba(255, 238, 151,' + op + ')),to(rgba(251, 228, 255,' + op + ')))';
-    
+
     const changeWidth = () => {
         wid = getOffset();
         op = getOpacity();
@@ -26,5 +26,20 @@ window.onload = () => {
         nav.style.backgroundImage = 'linear-gradient(90deg,rgba(255, 238, 151,' + op + '),rgba(251, 228, 255,' + op + '))';
         nav.style.backgroundImage = '-webkit-gradient(linear,left top,right top,from(rgba(255, 238, 151,' + op + ')),to(rgba(251, 228, 255,' + op + ')))';
     }
-    document.addEventListener('scroll',changeWidth);
+    document.addEventListener('scroll', changeWidth);
+
+    const card = document.querySelectorAll(".Card");
+
+    for(let i=0;i<card.length;i++){
+        card[i].addEventListener("mouseover", () => {
+            card[i].children[2].style.width = "100%";
+            card[i].children[2].style.height = "5px";
+            card[i].children[2].style.background = "linear-gradient(to right, rgb(255,240,0), rgb(255,210,0))";
+        });
+        card[i].addEventListener("mouseout", () => {
+            card[i].children[2].style.width = "0";
+            card[i].children[2].style.height = "0";
+            card[i].children[2].style.backgroundColor = "transparent";
+        });
+    }
 }
